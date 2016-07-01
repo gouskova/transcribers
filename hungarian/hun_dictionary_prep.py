e527f950672fbd7ab32630756e0be289b0cf9357
Ildi Szabo <szabo.ildikomse@gmail.com>
2016-07-01 11:26:44 -0400

    Update hun_dictionary_prep.py
    I took the ...prep.py and the ...parser.py apart, because I needed to make major modifications to the parser and the prep takes some time to run and I wanted to cut that part out to be able to modify the parser efficiently.

--- hungarian/hun_dictionary_prep.py
+++ hungarian/hun_dictionary_prep.py
@@ -1,8 +1,7 @@
-#!usr/bin/env python2.7
-# encoding: utf8
-# -*- coding: utf-8 -*
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# This is a script prepping a newline-eliminated .txt file into a proper input format for transcriber_hun.py
+# This is a script prepping a newline-eliminated .csv file of a fictionary into a list that doesn't contain illicit characters or
 
 #====================================================
 # Basic housekeeping: the original file I had is a dictionary, so quite a lot of words had superscript numbers to indicate different meanings; hyphenated compounds were also ruled out
@@ -10,55 +9,50 @@
 import csv
 wordlist = []
 wordlength = 0
-with open('C:\Users\ildi\Dropbox\NYELVESZET\PROJECTS\Tiers\Transcriber_hun\hun_dict_SzAB.csv') as f:
+bound_stems = []
+
+digraphs = ["cs", "dz", "dzs", "gy", "ly", "ny", "sz", "ty", "zs", "á", "é", "í", "ó", "ö", "ő", "ú", "ü", "ű"]
+digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+#banned_segments = [" ", ",", "-", "à", "ä"]
+
+with open('C:/Users/ildi/Dropbox/NYELVESZET/GitHub/transcribers/hungarian/hun_dict_SzAB.csv', encoding='utf-8') as f:
     reader = csv.reader(f, delimiter="\t")
     for row in reader:
         word = row[0]
-        word = word.replace('1', '')
-        word = word.replace('2', '')
-        word = word.replace('3', '')
-        word = word.replace('4', '')
-        word = word.replace('5', '')
-        if word not in wordlist:
-            if len(word) > 1 and word != 'cs' and word != 'dz' and word != 'dzs' and word != 'gy' and word != 'ly' and word != 'ny' and word != 'sz' and word != 'ty' and word != 'zs' and '-' not in word:
-                wordlength += len(word)
-                wordlist.append(word)
+        for digit in digits:
+            word = word.replace(digit,"")
+        #word = word.replace('1', '')
+        #word = word.replace('2', '')
+        #word = word.replace('3', '')
+        #word = word.replace('4', '')
+        #word = word.replace('5', '')
+        if word not in wordlist and not (word.startswith("leg") and word.endswith("bb")) and len(word) > 1 and word not in digraphs and " " not in word and "," not in word and "-" not in word and "à" not in word and "ä" not in word:
+            if word.endswith("a"):
+                alternant = word[0:len(word)-1]+"á"
+                bound_stems.append(alternant)
+            if word.endswith("e"):
+                alternant = word[0:len(word)-1]+"é"
+                bound_stems.append(alternant)
+            if word.endswith("alom"):
+                alternant = word[0:len(word)-4]+"alm"
+                bound_stems.append(alternant)
+            if word.endswith("elem"):
+                alternant = word[0:len(word)-4]+"elm"
+            wordlength += len(word)
+            wordlist.append(word.strip())
 
 print(str(len(wordlist)) + " words remaining")
 #print wordlist[:100]
-print float(wordlength)/len(wordlist)
+print(float(wordlength)/len(wordlist))
 
 #====================================================
-# Filtering out compounds and derived words: fake_suffixes is a collection of "words" that as second parts result in fake positives for compounds
+#Writing out the output file into a .csv file
 #====================================================
-roots = []
-rootlength = wordlength
 
-fake_suffixes = ['oda', 'dia', 'ha', 'ja', 'se', 'ne', 'le', 'de', 'ad', 'na', 'ma', 'la', 'tematika', 'ex', 'ú']
-preverbs = ['meg', 'le', 'fel', 'föl', 'be', 'ki', 'szét', 'össze', 'vissza', 'hozzá', 'rá', 'neki', 'át', 'oda', 'ide']
-suffixes = []
-# comment the line below out if you don't want to rule out derivative suffixes
-suffixes = ['zat', 'zet', 'ság', 'ség', 'ó', 'ő', 'adék', 'omány', 'emény', 'os', 'es', 'ás', 'és', 'i']
+with open('hun_wordlist.csv', 'w', encoding='utf-8', newline='') as wordlist_csv:
+    writer = csv.writer(wordlist_csv)
+    writer.writerows(zip(wordlist))
 
-for potential_compound in wordlist:
-    length = len(potential_compound)
-    roots.append(potential_compound)
-    for n in range(length):
-        potential_prefix = str(potential_compound[0:n])
-        potential_suffix = str(potential_compound[n:length])
-        if potential_prefix in wordlist and (potential_suffix in wordlist or potential_suffix in suffixes) and potential_suffix not in fake_suffixes:
-            #print potential_compound
-            roots.remove(potential_compound)
-            rootlength = rootlength - len(potential_compound)
-            break
-
-print(str(len(roots)) + " words remaining")
-#print roots[:100]
-print float(rootlength)/len(roots)
-
-#====================================================
-# write roots into a .csv file that can be an input to the transcriber
-#====================================================
-with open('hun_roots.csv', 'wb') as roots_csv:
-    writer = csv.writer(roots_csv)
-    writer.writerows(zip(roots))
+with open('hun_bound_stems.csv', 'w', encoding='utf-8', newline='') as bound_stems_csv:
+    writer = csv.writer(bound_stems_csv)
+    writer.writerows(zip(bound_stems))

17053742b86604fb5d7edd32be2e1c916e024a77
gouskova <gouskova@gmail.com>
2017-10-18 13:47:08 -0400

    quech transcriber

--- quechua/quechua_transcriber.py
+++ quechua/quechua_transcriber.py
@@ -20,9 +20,22 @@ def transcribe_wds(ipatable, infile, outfile):
     print('done')
 
 
-quechortho = '/home/maria/git/morphology/quechua/words/words_try3.txt'
-quechipa = '/home/maria/git/morphology/quechua/words/words_try3_ipa.txt'
 quech_ipa_table = '/home/maria/git/transcribers/quechua/quechua_transcription_key.txt'
+
+#newspaper words file, segmented:
+
+#quechortho = '/home/maria/git/morphology/quechua/words/words_try3.txt'
+#quechipa = '/home/maria/git/morphology/quechua/words/words_try3_ipa.txt'
+
+#roots file:
+quechortho = '/home/maria/git/morphology/quechua/roots/LearningData.txt'
+quechipa = '/home/maria/git/morphology/quechua/roots/roots_ipa.txt'
+
+#unsegmented novela:
+#quechortho = '/home/maria/git/morphology/quechua/words/PYW_novela.txt'
+#quechipa = '/home/maria/git/morphology/quechua/words/novela_ipa.txt'
+
+
 
 
 transcribe_wds(quech_ipa_table, quechortho, quechipa)

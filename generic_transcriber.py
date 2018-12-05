d5101ec6907c9be9658b0402f8f5234d30c6e0c7
gouskova <gouskova@gmail.com>
2018-12-05 14:37:45 -0500

    added option to transcribe multi-column files in generic_transcriber.py

--- generic_transcriber.py
+++ generic_transcriber.py
@@ -4,31 +4,50 @@ def transc_table(ipatable):
     transkey = {}
     with open(ipatable, 'r', encoding='utf-8') as f:
         for line in f:
-            ortho = line.strip().split('\t')[0]
-            ipa = line.strip().split('\t')[1]
-            transkey[ortho]=ipa
+            if not line=='':
+                ortho = line.strip().split('\t')[0]
+                ipa = line.strip().split('\t')[1]
+                transkey[ortho]=ipa
     return transkey
 
 
-def transcribe_wds(ipatable, infile, outfile):
+def transcribe_wds(ipatable, infile, outfile, takefirstcolumn=True):
     transkey = transc_table(ipatable)
     with open(infile, 'r', encoding='utf-8') as f:
         with open(outfile, 'w', encoding='utf-8') as out:
             for line in f:
-                word = line.strip().split('\t')[0].split(' ')
-                out.write(' '.join([transkey[ortho] if ortho in transkey else ortho for ortho in word])+ '\n')
+                if takefirstcolumn:
+                    word = line.strip().split('\t')[0].split(' ')
+                    out.write(' '.join([transkey[ortho] if ortho in transkey else ortho for ortho in word])+ '\n')
+                else:
+                    if '\t' in line:
+                        words = line.strip().split('\t')
+                        newwords = []
+                        for word in words:
+                            newwords.append(' '.join([transkey[ortho] if ortho in transkey else ortho for ortho in word.split(' ')]))
+                        out.write('\t'.join(newwords)+'\n')
+                    else:
+                        print("Sorry, files must have either one word per line or be in CSV tab-separated columnar format. If the latter, each word will be transcribed in the order it appears in the input file.")
+
     print('done')
 
 
 
 if __name__=='__main__':
     import sys
-    try:    
-        ipatable = sys.argv[1]
-        infile = sys.argv[2]
-        outfile = sys.argv[3]
-        transcribe_wds(ipatable, infile, outfile)
-        print ('your transcribed file is in ' + outfile)
+    try:
+        if 'help' in sys.argv:
+            print("usage:\n\n$ python3 generic_transcriber.py fullpathtoIPAtable fullpathtoinputfile fullpathtooutputfile\n\n\n if your data file has multiple columns, add 'False' after the other arguments. The script otherwise assumes that you have one line per word.\n\n\n the IPA table file should have two columns; one for orthographic representations and one for IPA correspondents, in that order.")
+        else:
+            ipatable = sys.argv[1]
+            infile = sys.argv[2]
+            outfile = sys.argv[3]
+            if 'False' in sys.argv:
+                transcribe_wds(ipatable, infile, outfile, takefirstcolumn=False)
+            else:
+                transcribe_wds(ipatable, infile, outfile)
+            print ('your transcribed file is in ' + outfile)
     except IndexError:
+        print(sys.argv)
         print ('Usage: python3 generic_transcriber.py fullpathtoIPAtable fullpathtoInfile fullpathtoOutfile')
 
--- quechua/quechua_transcriber.py
+++ quechua/quechua_transcriber.py
@@ -11,12 +11,16 @@ def transc_table(ipatable):
 
 
 def transcribe_wds(ipatable, infile, outfile):
+    '''
+    this only transcribes the first word on each line! assumes file to be tab-separated. comments, glosses, etc. will be left alone
+    '''
     transkey = transc_table(ipatable)
     with open(infile, 'r', encoding='utf-8') as f:
         with open(outfile, 'w', encoding='utf-8') as out:
             for line in f:
                 word = line.strip().split('\t')[0].split(' ')
-                out.write(' '.join([transkey[ortho] if ortho in transkey else ortho for ortho in word])+ '\n')
+                rest = '\t'.join(line.strip().split('\t')[1:])
+                out.write('\t'.join([' '.join([transkey[ortho] if ortho in transkey else ortho for ortho in word]), rest])+'\n')
     print('done')
 
 
@@ -24,8 +28,8 @@ quech_ipa_table = '/home/maria/git/transcribers/quechua/quechua_transcription_ke
 
 #newspaper words file, segmented:
 
-quechortho = '/home/maria/git/morphology/quechua/words/NC_type_seg.txt'
-quechipa = '/home/maria/git/morphology/quechua/words/words_ipa.txt'
+#quechortho = '/home/maria/git/morphology/quechua/words/NC_type_seg.txt'
+#quechipa = '/home/maria/git/morphology/quechua/words/words_ipa.txt'
 
 #roots file:
 #quechortho = '/home/maria/git/morphology/quechua/roots/LearningData.txt'
@@ -35,7 +39,10 @@ quechipa = '/home/maria/git/morphology/quechua/words/words_ipa.txt'
 #quechortho = '/home/maria/git/morphology/quechua/words/PYW_novela.txt'
 #quechipa = '/home/maria/git/morphology/quechua/words/novela_ipa.txt'
 
+#quechortho = '/home/maria/git/phonotactics/data/quechua/wds/LearningData.txt'
 
+quechortho = '/home/maria/git/phonotactics/data/quechua/wds/TestingData_pseudoorth.txt'
+quechipa = '/home/maria/Desktop/TestingData.txt'
 
 
 transcribe_wds(quech_ipa_table, quechortho, quechipa)

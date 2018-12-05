6f9554a09024c73772eecb2415c83e9555b7a74c
gouskova <gouskova@gmail.com>
2018-12-05 14:54:17 -0500

    test ipa file

--- generic_transcriber.py
+++ generic_transcriber.py
@@ -27,7 +27,9 @@ def transcribe_wds(ipatable, infile, outfile, takefirstcolumn=True):
                             newwords.append(' '.join([transkey[ortho] if ortho in transkey else ortho for ortho in word.split(' ')]))
                         out.write('\t'.join(newwords)+'\n')
                     else:
-                        print("Sorry, files must have either one word per line or be in CSV tab-separated columnar format. If the latter, each word will be transcribed in the order it appears in the input file.")
+                        if line=='':
+                            continue 
+
 
     print('done')
 
@@ -35,9 +37,10 @@ def transcribe_wds(ipatable, infile, outfile, takefirstcolumn=True):
 
 if __name__=='__main__':
     import sys
+    helpmessage="usage:\n\n$ python3 generic_transcriber.py fullpathtoIPAtable fullpathtoinputfile fullpathtooutputfile\n\n\n if your data file has multiple columns, add 'False' after the other arguments. The script otherwise assumes that you have one line per word.\n\n\n the IPA table file should have two columns; one for orthographic representations and one for IPA correspondents, in that order."
     try:
         if 'help' in sys.argv:
-            print("usage:\n\n$ python3 generic_transcriber.py fullpathtoIPAtable fullpathtoinputfile fullpathtooutputfile\n\n\n if your data file has multiple columns, add 'False' after the other arguments. The script otherwise assumes that you have one line per word.\n\n\n the IPA table file should have two columns; one for orthographic representations and one for IPA correspondents, in that order.")
+            print(helpmessage)
         else:
             ipatable = sys.argv[1]
             infile = sys.argv[2]
@@ -49,5 +52,5 @@ if __name__=='__main__':
             print ('your transcribed file is in ' + outfile)
     except IndexError:
         print(sys.argv)
-        print ('Usage: python3 generic_transcriber.py fullpathtoIPAtable fullpathtoInfile fullpathtoOutfile')
+        print (helpmessage)
 

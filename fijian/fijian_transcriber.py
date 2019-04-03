e8383ec245175b755f9cc32f5db89854e8248a6b
gouskova <gouskova@gmail.com>
2019-04-03 19:40:08 -0400

    backup

--- fijian/fijian_transcriber.py
+++ fijian/fijian_transcriber.py
@@ -10,31 +10,73 @@ import generic_transcriber
 
 ipadic = generic_transcriber.transc_table('fijian_ipa_key.txt')
 
-digraphs = {'d i': 'n ʤ i', 'd r': 'n r', 't i': 'ʧ i'}
+seg_digraphs = {'d i': 'nʤ i', 'd r': 'nr', 't i': 'ʧ i'}
+#has affricates, prenasalized stops
 
+clust_digraphs = {'d i': 'n d ʒ i', 'd r': 'n r', 't i': 't ʃ i'}
+#has no complex segs
 
-def transcribe_wd(word):
-    word.replace("j", "ʧ") 
-    for dig in digraphs:
-        word = word.replace(dig, digraphs[dig])
+affr_digraphs = {'d i': 'n ʤ i', 'd r': 'n r', 't i': 'ʧ i'}
+#has affricates but not prenasalized stops
+
+def transcribe_seg(word):
+    word = word.replace("j", "ʧ")
+    for dig in seg_digraphs:
+        word = word.replace(dig, seg_digraphs[dig])
+    word = word.replace("b", "mb")
+    word = word.replace("d", "nd")
+    word = word.replace("q", "ŋɡ")
     for let in ipadic:
         word = word.replace(let, ipadic[let])
     return word
 
-def transcribe_corpus(infile, outfile):
+
+def transcribe_clust(word):
+    word = word.replace("j", "t ʃ") 
+    for dig in clust_digraphs:
+        word = word.replace(dig, clust_digraphs[dig])
+    word = word.replace("b", "m b")
+    word = word.replace("q", "ŋ ɡ")
+    word = word.replace("d", "n d")
+    word = word.replace("n n", "n")
+    for let in ipadic:
+        word = word.replace(let, ipadic[let])
+    return word
+
+
+
+def transcribe_affr(word):
+    word = word.replace("j", "ʧ")
+    for dig in affr_digraphs:
+        word = word.replace(dig, affr_digraphs[dig])
+    word = word.replace("b", "m b")
+    word = word.replace("d", "n d")
+    word = word.replace("q", "ŋ ɡ")
+    for let in ipadic:
+        word = word.replace(let, ipadic[let])
+    return word
+
+
+def transcribe_wd(word, kind):
+    if kind == 'seg':
+        return transcribe_seg(word)
+    if kind == 'clust':
+        return transcribe_clust(word)
+    if kind == 'affr':
+        return transcribe_affr(word)
+
+
+def transcribe_corpus(infile, outfile, kind):
     with open(infile, 'r', encoding='utf-8') as fi:
         with open(outfile, 'w', encoding='utf-8') as out:
             for line in fi:
-                out.write(transcribe_wd(line))
+                out.write(transcribe_wd(line, kind))
     print('done')
 
 
 if __name__=='__main__':
-    if len(sys.argv)>1:
-        infile = os.path.join(os.getcwd(), sys.argv[1])
-        outfile = os.path.join(os.getcwd(), sys.argv[2])
-    else:
-        infile = os.path.join(os.getcwd(), "fijian_crubadan.txt")
-        outfile = os.path.join(os.getcwd(), "LearningData.txt")
-    transcribe_corpus(infile, outfile)
+    infile = os.path.join(os.getcwd(), sys.argv[1])
+    outfile = os.path.join(os.getcwd(), '_'.join(['LearningData', sys.argv[2]+'.txt']))
+    transcribe_corpus(infile, outfile, sys.argv[2])
+    
 

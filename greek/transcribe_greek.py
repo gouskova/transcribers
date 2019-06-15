7e5e9645a643be83e7049547bf15668fb521bd14
gouskova <gouskova@gmail.com>
2019-06-15 12:16:45 -0400

    changed some transcription details after checking sources

--- greek/transcribe_greek.py
+++ greek/transcribe_greek.py
@@ -3,7 +3,12 @@
 
 '''
 slight modifications to the file from Schott's wordlist for greek (Ralf's dictionaries) to make it usable in UCLAPL research
+
+also, some utilities for converting greek orthography into 
 '''
+
+import re
+
 
 def writefeats(inset, outpath):
     outlist = []
@@ -15,13 +20,25 @@ def writefeats(inset, outpath):
     with open(outpath, 'w', encoding='utf-8') as f:
         f.write('\n'.join(sorted(outlist)))
 
+def get_letters(inpath):
+    outlist = set()
+    with open(inpath, 'r', encoding='utf-8') as f:
+        for line in f:
+            word = set(line.strip('\n').lower())
+            outlist=word|outlist
+    with open('transcription_key.txt', 'w', encoding='utf-8') as out:
+        out.write('\n'.join(sorted(list(outlist))))
+    print("done collecting letters")
+
 
 def spacify(inpath):
     out = set() 
     with open(inpath, 'r', encoding='utf-8') as f:
         for line in f:
             wd = ' '.join(list(line.rstrip('\n')))
+            wd = wd.replace("ʎ ʎ", "ʎ") #mistake in the orig transcription file 
             wd = wd.replace('ɪ', 'i')
+            wd = wd.replace("ŋ g", "g")
             out.add(wd)
     return out
 
@@ -32,8 +49,20 @@ def writeld(inset, outpath):
             f.write(wd+'\n')
 
 
+def transcribe_greek_ortho(word, transkey, digraphkey):
+    '''
+    strip /f, /n 
+    '''
+    word = word.rstrip('/fnbced')
+    for seg in word:
+        pass
+
 if __name__=='__main__':
-    inpath = 'greek-dictionary.txt'
-    x = spacify(inpath)
-    writeld(x, 'LearningData.txt')
-    writefeats(x, 'Features.txt')
+    import sys
+    if 'ralf' in sys.argv:
+        inpath = 'greek-dictionary.txt'
+        x = spacify(inpath)
+        writeld(x, 'LearningData.txt')
+        writefeats(x, 'Features.txt')
+    elif 'sourceforge_greek.txt' in sys.argv:
+        get_letters('sourceforge_greek.txt')

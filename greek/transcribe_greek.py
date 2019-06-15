15aec84a557956d929b4fc217b596a75a2e99f68
gouskova <gouskova@gmail.com>
2019-06-15 13:05:13 -0400

    crubadan

--- greek/transcribe_greek.py
+++ greek/transcribe_greek.py
@@ -32,6 +32,9 @@ def get_letters(inpath):
 
 
 def spacify(inpath):
+    '''
+    returns a list of words 
+    '''
     out = set() 
     with open(inpath, 'r', encoding='utf-8') as f:
         for line in f:
@@ -49,13 +52,56 @@ def writeld(inset, outpath):
             f.write(wd+'\n')
 
 
-def transcribe_greek_ortho(word, transkey, digraphkey):
-    '''
-    strip /f, /n 
-    '''
-    word = word.rstrip('/fnbced')
+def get_key(inpath):
+    out = {}
+    with open(inpath, 'r', encoding='utf-8') as f:
+        for line in f:
+            segs = line.strip().split('\t')
+            out[segs[0]] = segs[1]
+    return out
+
+
+def transcribe(word, digraphs, unis):
     for seg in word:
-        pass
+        word = ''.join([digraphs[seq] if seq in digraphs else seq for seq in word])
+        word = ' '.join([unis[seg] if seg in unis else seg for seg in word])
+    return word
+
+
+def transcribe_words(inpath, outpath):
+    digraphs = get_key('digraphs.txt')
+    unis = get_key('transcription_key.txt')
+    with open(inpath, 'r', encoding='utf-8') as f:
+        with open(outpath, 'w', encoding='utf-8') as out:
+            for line in f:
+                word = line.strip().split(' ')[0].lower()
+                word = word.rstrip('/fnbced')
+                word = transcribe(word, digraphs, unis)
+                out.write(word+'\n')
+    print('done writing ld')
+
+
+def cheat(ralfpath, crubapath, outpath):
+    out = {}
+    with open(ralfpath, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.rstrip('\n').split('\t')
+            wd = line[0]
+            trans = ' '.join(list(line[1]))
+            trans = trans.replace("ʎ ʎ", "ʎ") #mistake in the orig transcription file 
+            trans = trans.replace('ɪ', 'i')
+            trans = trans.replace("ŋ g", "g")
+            out[wd] = trans
+    with open(crubapath, 'r', encoding='utf-8') as f:
+        with open(outpath, 'w', encoding='utf-8') as outf:
+            for line in f:
+                wd = line.rstrip('\n').split(' ')[0]
+                if wd in out:
+                    outf.write(out[wd]+'\n')
+                else:
+                    print(wd)
+
+
 
 if __name__=='__main__':
     import sys
@@ -66,3 +112,6 @@ if __name__=='__main__':
         writefeats(x, 'Features.txt')
     elif 'sourceforge_greek.txt' in sys.argv:
         get_letters('sourceforge_greek.txt')
+    elif 'crubadan' in sys.argv:
+        #transcribe_words('greek_an_crubadan.txt', 'LearningData_crub.txt')
+        cheat('greek-dictionary_orig.txt', 'greek_an_crubadan.txt', 'LearningData_crub.txt')
